cb85bee5f002712983d370a26b1f69000ae3c94e
ADEsposito <a.desposito640@gmail.com>
2017-02-23 12:46:00 -0600

    add functionality to compare mouse click location and zombie locations, determine if it was a hit or miss, remove the appropriate zombie if it was a hit, update score/shots/hits counters, stop animation if shots left reaches 0, display final score in header of page, other minor tweaks for style

--- js/app.js
+++ js/app.js
@@ -4,12 +4,14 @@ var scoreCounter = 0;
 var shotsLeftCounter = 10;
 var successfulHitsCounter = 0;
 
+var zombieIndex = 0;
+
 var ctx;
 var backgroundImage;
 var zombieImage;
 var x = 0;
 var y = 0;
-var numOfZombies = 5;
+var numOfZombies = 10;
 var fallingZombies = [];
 
 function getMousePos(canvas, evt) {
@@ -24,14 +26,67 @@ function drawBackground() {
   ctx.drawImage(backgroundImage, 0, 0);
 }
 
+canvas.addEventListener('click', function(event) {
+  lastDownTarget = event.target;
+  var mousePos = getMousePos(canvas, event);
+  var clickX = mousePos.x;
+  var clickY = mousePos.y;
+  checkZombieClick(clickX, clickY)
+  }, false);
+
+
+function checkZombieClick(mouseX, mouseY){
+  var xCheck = false;
+  var yCheck = false;
+
+  for(i = 0; i < fallingZombies.length; i++) {
+
+    var rightBoundX = updateFallingZombies()[i].x + 100;
+    var upperBoundY = updateFallingZombies()[i].y;
+    var leftBoundX = updateFallingZombies()[i].x;
+    var lowerBoundY = updateFallingZombies()[i].y + 100;
+
+    if (((mouseX <= rightBoundX) && (mouseX >= leftBoundX)) && ((mouseY >= upperBoundY) && (mouseY <= lowerBoundY))) {
+      xCheck = true;
+      yCheck = true;
+
+      zombieIndex = i;
+    }
+  }
+
+
+  if (xCheck === true && yCheck === true) {
+      shotsLeftCounter--;
+      successfulHitsCounter += 1;
+      scoreCounter += 100;
+      fallingZombies.splice(zombieIndex, 1)
+      numOfZombies--
+    }
+    else {
+      shotsLeftCounter--;
+    }
+    $('#shots').text("SHOTS LEFT : " + shotsLeftCounter);
+    $('#hits').text("HITS : " + successfulHitsCounter);
+    $('#score').text("SCORE : " + scoreCounter);
+}
+
+
+function updateFallingZombies(){
+  return fallingZombies
+}
+
 function draw() {
+  if(shotsLeftCounter === 0){
+    $('#shots').text("SHOTS LEFT : 0");
+    $('#banner').text("FINAL SCORE : " + scoreCounter);
+    clearInterval(animate)
+  }
   // clear the canvas
   ctx.clearRect(0,0, canvas.width,canvas.height);
   drawBackground();
 
   for (var i=0; i< numOfZombies; i++) {
-    // the falling zombies
-    ctx.drawImage (fallingZombies[i].image, fallingZombies[i].x, fallingZombies[i].y);
+    ctx.drawImage (updateFallingZombies()[i].image, updateFallingZombies()[i].x, updateFallingZombies()[i].y);
 
     // set speed of falling for zombies
     fallingZombies[i].y += fallingZombies[i].speed;
@@ -42,35 +97,18 @@ function draw() {
       // zombies appear randomly along width of canvas
       fallingZombies[i].x = Math.random() * 1200;
     }
-
-    //console.log(fallingZombies[i].x);
-    //console.log(fallingZombies[i].y);
-
-    // listen for clicks on canvas
-    canvas.addEventListener('click', function(event) {
-      lastDownTarget = event.target;
-      console.log("click worked");
-      var mousePos = getMousePos(canvas, event);
-      var clickX = mousePos.x;
-      var clickY = mousePos.y;
-      console.log(clickX + " is the click X " + clickY + " is the click Y");
-      }, false);
-
-    
   }
 }
 
 function setup() {
   var canvas = document.getElementById('canvas');
 
-
   if (canvas.getContext) {
     ctx = canvas.getContext('2d');
-// deleted event listener from here... it worked
 
     backgroundImage = new Image();
-    backgroundImage.src = "http://img11.deviantart.net/9f53/i/2013/023/7/4/hospital_corridor__1_by_montstar-d5sfata.jpg";
-    setInterval(draw, 36);
+    backgroundImage.src = "https://static1.squarespace.com/static/512c621be4b03f854ef23109/515e08eee4b0f7be5031f2ae/51783cc3e4b054c7ac3a3c15/1404352428072/Hospitals++38.jpg?format=1500w";
+
     for (var i = 0; i < numOfZombies; i++) {
       var fallingZ = new Object();
       fallingZ["image"] =  new Image();
@@ -80,86 +118,16 @@ function setup() {
       fallingZ["y"] = Math.random() * 5;
       fallingZ["speed"] = 3 + Math.random() * 5;
       fallingZombies.push(fallingZ);
-
     }
   }
 }
+
+// TRY TO FLESH THIS OUT RATHER THAN USE SETINTERVAL
+/* function animate(){
+
+  window.requestAnimationFrame(animate)
+}
+*/
 
 setup();
-/*
-************* INITIAL START TO LOGIC, PLACES ONE IMAGE IN STATIC POSITION, LISTENS FOR CLICK, RESPONDS APPROPRIATELY IF IS A HIT OR MISS *************
-var randomXLocation = function() {
-  console.log("X")
-  var xMinimum = 0;
-  var xMaximum = 600;
-  randomX = Math.floor(Math.random() * (xMaximum - xMinimum + 1)) + xMinimum;
-}
-
-var randomYLocation = function() {
-  console.log("Y")
-  var yMinimum = 0;
-  var yMaximum = 600;
-  randomY = Math.floor(Math.random() * (yMaximum - yMinimum + 1) + yMinimum);
-}
-
-
-var generateImageLocation = function() {
-  randomXLocation();
- randomYLocation();
-
-  var canvas = document.getElementById("canvas");
-  var ctx = canvas.getContext("2d");
-  var image = document.getElementById("zombie");
-  ctx.drawImage(image, randomX, randomY, 50, 50);
-  var rightBoundX = randomX + 50;
-  var upperBoundY = randomY;
-  var leftBoundX = randomX;
-  var lowerBoundY = randomY + 50;
-  var xCheck = false;
-  var yCheck = false;
-
-  canvas.addEventListener('click', function(event) {
-    console.log("clicked zombie");
-    //console.log(randomX + "is the X of the image " + randomY + " is the Y of the image");
-    var mousePos = getMousePos(canvas, event);
-    var clickX = mousePos.x;
-    var clickY = mousePos.y;
-    //console.log(clickX + " is the click X " + clickY + " is the click Y");
-/*
-    //image is 50 x 50 px
-    //coordinates of image start at top left corner (randomX & randomY)
-    //max X of click must be between randomX & randomX + 50
-    //max Y of click must be between randomY & randomY - 50
-    if ((clickX <= rightBoundX) && (clickX >= leftBoundX)) {
-      //console.log("X Check");
-      //console.log(rightBoundX + " is the Right Edge. " + leftBoundX + " is the Left Edge.")
-      xCheck = true;
-    }
-    if ((clickY >= upperBoundY) && (clickY <= lowerBoundY)) {
-      //console.log("Y Check");
-      //console.log(upperBoundY + " is the Upper Edge. " + lowerBoundY + " is the Lower Edge.")
-      yCheck = true;
-    }
-
-
-    if (xCheck === true && yCheck === true) {
-      console.log("Hit!");
-      shotsLeftCounter -= 1;
-      successfulHitsCounter += 1;
-      scoreCounter += 100;
-      console.log(shotsLeftCounter + " shots left. " + successfulHitsCounter + " successful hits. " + scoreCounter + " is your score.")
-    }
-    else {
-      console.log("Miss!");
-      shotsLeftCounter -= 1;
-      console.log(shotsLeftCounter + " shots left. " + successfulHitsCounter + " successful hits. " + scoreCounter + " is your score.")
-    }
-    $('#shots').text("SHOTS LEFT : " + shotsLeftCounter);
-    $('#hits').text("HITS : " + successfulHitsCounter);
-    $('#score').text("SCORE : " + scoreCounter);
-    */
-/*  })
-  }
-
-generateImageLocation();
-*/
+var animate = setInterval(draw, 36);
